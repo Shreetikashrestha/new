2dc6d11bc5aecf8b0a516e494a83582f2a932cde
ShreetikaShrestha <shreetika592gmail.com>
2025-02-23 19:38:06 +0545

    nailcare

--- src/pages/servicepages/NailCare.jsx
+++ src/pages/servicepages/NailCare.jsx
@@ -17,42 +17,42 @@ const services = [
     title: "Manicure",
     description:
       "Our manicure services include nail shaping, cuticle care, and a relaxing hand massage. Choose from a variety of polish colors to complete your look.",
-    image: "/api/placeholder/400/300",
+    image: "https://res.cloudinary.com/dawklhf0q/image/upload/v1739426972/Spa-Manicure-Deluxe_rwkuwo.jpg",
   },
   {
     id: 2,
     title: "Pedicure",
     description:
       "Enjoy a soothing pedicure that includes nail trimming, cuticle care, and a foot massage. Finish with your choice of polish for beautiful, healthy feet.",
-    image: "/api/placeholder/400/300",
+    image: "https://res.cloudinary.com/dawklhf0q/image/upload/v1739426981/pedicure-types_zvclak.jpg",
   },
   {
     id: 3,
     title: "Gel Nails",
     description:
       "Get long-lasting, chip-resistant nails with our gel nail services. Choose from a wide range of colors and designs for a flawless finish.",
-    image: "/api/placeholder/400/300",
+    image: "https://res.cloudinary.com/dawklhf0q/image/upload/v1739427001/4b25005118cc3212833dddb6aa5e8dfb_oxdakq.jpg",
   },
   {
     id: 4,
     title: "Acrylic Nails",
     description:
       "Enhance your nails with our durable acrylic nail services. Perfect for those looking for added length and strength.",
-    image: "/api/placeholder/400/300",
+    image: "https://res.cloudinary.com/dawklhf0q/image/upload/v1739427043/61xumms8DML._AC_UF1000_1000_QL80__k1vsyw.jpg",
   },
   {
     id: 5,
     title: "Nail Art",
     description:
       "Express your creativity with our custom nail art services. From simple designs to intricate patterns, our artists can create the perfect look for you.",
-    image: "/api/placeholder/400/300",
+    image: "https://res.cloudinary.com/dawklhf0q/image/upload/v1739427978/a80c7dc04eabfb2172230caf96bbb128_wlwoyq.jpg",
   },
   {
     id: 6,
     title: "Nail Treatments",
     description:
       "Keep your nails healthy and strong with our range of nail treatments. From strengthening to moisturizing, we have the perfect solution for your nail care needs.",
-    image: "/api/placeholder/400/300",
+    image: "https://res.cloudinary.com/dawklhf0q/image/upload/v1739428002/Before-After-Nail_1024x1024_2x_lxhers.jpg",
   },
 ];
 
